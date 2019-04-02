62b35441819561080cd6040dd7f2460c6f09bcf3
Shachar Langbeheim <shachar@lightricks.com>
2019-04-02 11:15:45 +0300

    use yuv

--- openGLShading.h
+++ openGLShading.h
@@ -2,6 +2,6 @@
 
 extern void setupOpenGL(int width, int height);
 
-extern void invertFrame(uint8_t *buffer, int width, int height);
+extern void invertFrame(uint8_t *y, uint8_t *u, uint8_t *v, uint8_t *output, int width, int height);
 
 extern void tearDownOpenGL(void);
