2fb2648176e74e70ea1c5dc47882d0fe34f5a9eb
Shachar Langbeheim <shachar@lightricks.com>
2019-05-14 17:02:04 +0300

    Update main.

--- Makefile
+++ Makefile
@@ -1,9 +1,25 @@
 VIDEO_URL := http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4
 
-transcoding: clean
-	g++ -g -Wall -std=c++14 all.hpp -o ./build/all.hpp.gch \
-	&& g++ -g -std=c++14 -Wall -o build/transcoding -lboost_date_time -lavformat -lavcodec -lswscale -lz -lglfw -lavutil -framework OpenGL *.cpp -I ./build -include all.hpp \
-	  && ./build/transcoding ./movies/small_bunny_1080p_60fps.mp4 ./movies/dog.mp4  ./movies/bunny_1s_gop.mp4 0.5 1 4
+ARGS=./movies/small_bunny_1080p_60fps.mp4 ./movies/dog.mp4  ./movies/bunny_1s_gop.mp4 0.5 100
+
+transcoding: ./*.cpp ./Transcoding/*.cpp
+	@g++ -g -w -std=c++17 all.hpp -o ./build/all.hpp.gch \
+	&& g++ -g -std=c++17 -w -o build/transcoding-apple-simd -lboost_date_time -lavformat -lavcodec -lswscale -lz -lglfw -lavutil -framework OpenGL *.cpp ./Transcoding/*.cpp -I./ -I ./build -include all.hpp \
 
 clean:
-	rm -rf ./build/*
+	@rm -rf ./build/*
+
+run-all:
+	@./build/transcoding-cpu ${ARGS} &&\
+	@./build/transcoding-gpu ${ARGS} &&\
+	@./build/transcoding-xsimd ${ARGS} &&\
+	@./build/transcoding-apple-simd .${ARGS} &&\
+	@./build/transcoding-cpu ${ARGS} &&\
+	@./build/transcoding-gpu ${ARGS} &&\
+	@./build/transcoding-xsimd ${ARGS} &&\
+	@./build/transcoding-apple-simd .${ARGS} &&\
+	@./build/transcoding-cpu ${ARGS}&&\
+	@./build/transcoding-gpu ${ARGS} &&\
+	@./build/transcoding-xsimd ${ARGS} &&\
+	@./build/transcoding-apple-simd ${ARGS}
+
--- main.cpp
+++ main.cpp
@@ -4,127 +4,139 @@
 #include <stdlib.h>
 #include <string.h>
 #include "./openGLShading.h"
-#include "WREDecoders.hpp"
-#include "WREEncoder.hpp"
+#include "Transcoding/Decoders.hpp"
+#include "Transcoding/Encoder.hpp"
 extern "C" {
 #include <libavutil/log.h>
 }
 
+using namespace WRETranscoding;
+
 #define FRONTEND 0;
 
-static void invert_single_frame(WREVideoDecoder *decoder, WREEncoder *encoder, uint32_t textureID) {
-  loadTexture(textureID, decoder->get_width(), decoder->get_height(), decoder->get_rgb_buffer());
+static void invert_single_frame(Encoder *encoder, uint32_t textureID) {
   invertFrame(textureID);
-  getCurrentResults(encoder->get_width(), encoder->get_height(), encoder->get_rgb_buffer());
+  encoder->write_to_rgb_buffer([&](uint8_t *buffer) {
+    getCurrentResults(encoder->get_width(), encoder->get_height(), buffer);
+  });
 }
 
-static void blend_frames(WREVideoDecoder *decoder, WREVideoDecoder *secondary_decoder,
-                         WREEncoder *encoder, uint32_t texture1ID, uint32_t texture2ID) {
-  loadTexture(texture1ID, decoder->get_width(), decoder->get_height(), decoder->get_rgb_buffer());
-  loadTexture(texture2ID, secondary_decoder->get_width(), secondary_decoder->get_height(),
-              secondary_decoder->get_rgb_buffer());
+static void blend_frames(Encoder *encoder, uint32_t texture1ID, uint32_t texture2ID) {
   blendFrames(texture1ID, texture2ID);
-  getCurrentResults(encoder->get_width(), encoder->get_height(), encoder->get_rgb_buffer());
+  encoder->write_to_rgb_buffer([&](uint8_t *buffer) {
+    getCurrentResults(encoder->get_width(), encoder->get_height(), buffer);
+  });
 }
 
 int main(int argc, char *argv[]) {
   av_log_set_level(AV_LOG_FATAL);
+  double blend_time = 0;
+  double invert_time = 0;
+  double texture_loading = 0;
+  double initial_start = clock();
   log_debug("start");
-  int height = 1200;
+  int height = 1080;
   int width = 1920;
   auto expected_framerate = 30;
-
-  auto decoder = new WREVideoDecoder(argv[1], expected_framerate, 1, 5, 2);
-  if (decoder == nullptr) {
-    log_error("error while preparing input");
-    return -1;
-  }
-
-  log_info("secondary decoder");
-  auto secondary_decoder = new WREVideoDecoder(argv[2], expected_framerate);
-  if (secondary_decoder == nullptr) {
-    log_error("error while preparing secondary input");
-    return -1;
-  }
-
-  log_info("audio decoder");
-  auto audio_decoder = new WREAudioDecoder(argv[1], 1, 4, 2);
-  if (audio_decoder == nullptr) {
-    log_error("error while preparing audio input");
-    return -1;
-  }
-
-  WREEncoder *encoder = new WREEncoder(argv[3], "libx264", width, height, expected_framerate,
-                                       audio_decoder->get_transcoding_components());
-
-  log_info("next");
+  log_info("setup openGL");
   float blendRatio = strtof(argv[4], NULL);
-  printf("blend ratio: %f\n", blendRatio);
-  int wait = atoi(argv[5]);
-  int length = atoi(argv[6]);
-
   setupOpenGL(width, height, blendRatio, NULL);
   uint32_t tex1 = createTexture();
   uint32_t tex2 = createTexture();
 
-  double source_time_base = decoder->get_time_base();
-  double duration_in_seconds = secondary_decoder->get_duration();
-  double maxTime = (length > duration_in_seconds ? duration_in_seconds : length) + wait;
-  printf("duration: %lf, max time: %lf, wait: %d\n", duration_in_seconds, maxTime, wait);
-  int response;
+  string name = "Apple SIMD";
 
-  long counted_frames = 0;
-  long blended_frames = 0;
-  long inverted_frames = 0;
-  long audio_frames = 0;
-  while (decoder->decode_next_frame() >= 0) {
-    counted_frames++;
+  int iterations = atoi(argv[5]);
+  for (int k = 0; k < iterations; k++) {
+    auto decoder = new VideoDecoder(argv[1], expected_framerate);
+    if (decoder == nullptr) {
+      log_error("error while preparing input");
+      return -1;
+    }
 
-    auto point_in_time = source_time_base * decoder->get_current_timestamp();
-    // logging("point in time %f", point_in_time);
-    if (point_in_time < wait || point_in_time > maxTime) {
-      inverted_frames++;
-      invert_single_frame(decoder, encoder, tex1);
+    log_info("secondary decoder");
+    auto secondary_decoder = new VideoDecoder(argv[2], expected_framerate);
+    if (secondary_decoder == nullptr) {
+      log_error("error while preparing secondary input");
+      return -1;
+    }
+
+    log_info("audio decoder");
+    auto audio_decoder = new AudioDecoder(argv[1]);
+    if (audio_decoder == nullptr) {
+      log_error("error while preparing audio input");
+      return -1;
+    }
+
+    auto encoder = new Encoder(argv[3], "libx264", width, height, expected_framerate,
+                               audio_decoder->get_transcoding_components());
+
+    double source_time_base = decoder->get_time_base();
+    int response;
+
+    long counted_frames = 0;
+    long blended_frames = 0;
+    long inverted_frames = 0;
+    long audio_frames = 0;
+    double start;
+    int frames = 0;
+
+    while (decoder->decode_next_frame() >= 0 && secondary_decoder->decode_next_frame() >= 0) {
+      start = clock();
+      decoder->read_from_rgb_buffer([&](const uint8_t *buffer) {
+        loadTexture(tex1, decoder->get_width(), decoder->get_height(), buffer);
+      });
+      secondary_decoder->read_from_rgb_buffer([&](const uint8_t *buffer) {
+        loadTexture(tex2, secondary_decoder->get_width(), secondary_decoder->get_height(), buffer);
+      });
+      texture_loading += clock() - start;
+
+      start = clock();
+      blend_frames(encoder, tex1, tex2);
+      blend_time += clock() - start;
+
+      start = clock();
+      invert_single_frame(encoder, tex1);
+      invert_time += clock() - start;
+
       encoder->encode_video_frame(source_time_base, decoder->get_current_timestamp());
-      // av_frame_unref(inputFrame);
-      continue;
     }
 
-    response = secondary_decoder->decode_next_frame();
-    if (response < 0) {
-      log_error("DECODER: Error while receiving a frame from the secondary decoder: %d", response);
-      return response;
+    double audio_time_base = audio_decoder->get_time_base();
+    while (audio_decoder->decode_next_frame() == 0) {
+      audio_frames++;
+      if (encoder->encode_audio_frame(audio_time_base, audio_decoder->get_current_timestamp()) !=
+          0) {
+        log_error("audio encoding error");
+      }
     }
 
-    blended_frames++;
-    blend_frames(decoder, secondary_decoder, encoder, tex1, tex2);
-    encoder->encode_video_frame(source_time_base, decoder->get_current_timestamp());
+    encoder->finish_encoding();
+
+    log_info("wrote %lu frames. %lu blended, %lu inverted, %lu audio", counted_frames,
+             blended_frames, inverted_frames, audio_frames);
+
+    log_debug("releasing all the resources");
+
+    delete (decoder);
+    log_debug("first decoder");
+    delete (secondary_decoder);
+    log_debug("second decoder");
+    delete (audio_decoder);
+    log_debug("releasing decoders");
+    delete (encoder);
+    log_debug("releasing encoder");
   }
-
-  double audio_time_base = audio_decoder->get_time_base();
-  while (audio_decoder->decode_next_frame() == 0) {
-    audio_frames++;
-    if (encoder->encode_audio_frame(audio_time_base, audio_decoder->get_current_timestamp()) != 0) {
-      log_error("audio encoding error");
-    }
-  }
-
-  encoder->finish_encoding();
-
-  log_info("wrote %lu frames. %lu blended, %lu inverted, %lu audio", counted_frames, blended_frames,
-           inverted_frames, audio_frames);
-
-  log_debug("releasing all the resources");
-
-  delete (decoder);
-  log_debug("first decoder");
-  delete (secondary_decoder);
-  log_debug("second decoder");
-  delete (audio_decoder);
-  log_debug("releasing decoders");
-  // delete (encoder);
-  log_debug("releasing encoder");
   tearDownOpenGL();
   log_debug("teardown opengl");
+
+  blend_time = blend_time / CLOCKS_PER_SEC / iterations;
+  invert_time = invert_time / CLOCKS_PER_SEC / iterations;
+  texture_loading = texture_loading / CLOCKS_PER_SEC / iterations / 2;
+  double total = (clock() - initial_start) / CLOCKS_PER_SEC / iterations;
+
+  printf("\n\n%s: texture loading: %f, blend: %f, invert: %f, total: %f\n\n\n", name.c_str(),
+         texture_loading, blend_time, invert_time, total);
+
   return 0;
 }
