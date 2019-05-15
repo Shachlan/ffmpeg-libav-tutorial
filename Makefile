0d329f4f195ad47e33e836a924270153e33073a4
Shachar Langbeheim <shachar@lightricks.com>
2019-05-15 10:42:00 +0300

    temp

--- Makefile
+++ Makefile
@@ -1,25 +1,26 @@
 VIDEO_URL := http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4
 
-ARGS=./movies/small_bunny_1080p_60fps.mp4 ./movies/dog.mp4  ./movies/bunny_1s_gop.mp4 0.5 100
+ARGS=./movies/small_bunny_1080p_60fps.mp4 ./movies/dog.mp4  ./build2/bunny_1s_gop.mp4 0.5 1
 
 transcoding: ./*.cpp ./Transcoding/*.cpp
-	@g++ -g -w -std=c++17 all.hpp -o ./build/all.hpp.gch \
-	&& g++ -g -std=c++17 -w -o build/transcoding-apple-simd -lboost_date_time -lavformat -lavcodec -lswscale -lz -lglfw -lavutil -framework OpenGL *.cpp ./Transcoding/*.cpp -I./ -I ./build -include all.hpp \
+	@g++ -g -w -std=c++17 all.hpp -o ./build2/all.hpp.gch \
+	&& g++ -g -std=c++17 -w -o build2/transcoding-apple-simd -lboost_date_time -lavformat -lavcodec -lswscale -lz -lglfw -ltbb -lavutil -framework OpenGL *.cpp ./Transcoding/*.cpp -I./ -I ./build2 -include all.hpp &&\
+	./build2/transcoding-apple-simd ${ARGS}
 
 clean:
-	@rm -rf ./build/*
+	@rm -rf ./build2/*
 
 run-all:
-	@./build/transcoding-cpu ${ARGS} &&\
-	@./build/transcoding-gpu ${ARGS} &&\
-	@./build/transcoding-xsimd ${ARGS} &&\
-	@./build/transcoding-apple-simd .${ARGS} &&\
-	@./build/transcoding-cpu ${ARGS} &&\
-	@./build/transcoding-gpu ${ARGS} &&\
-	@./build/transcoding-xsimd ${ARGS} &&\
-	@./build/transcoding-apple-simd .${ARGS} &&\
-	@./build/transcoding-cpu ${ARGS}&&\
-	@./build/transcoding-gpu ${ARGS} &&\
-	@./build/transcoding-xsimd ${ARGS} &&\
-	@./build/transcoding-apple-simd ${ARGS}
+	@./build2/transcoding-cpu ${ARGS} &&\
+	@./build2/transcoding-gpu ${ARGS} &&\
+	@./build2/transcoding-xsimd ${ARGS} &&\
+	@./build2/transcoding-apple-simd ${ARGS} &&\
+	@./build2/transcoding-cpu ${ARGS} &&\
+	@./build2/transcoding-gpu ${ARGS} &&\
+	@./build2/transcoding-xsimd ${ARGS} &&\
+	@./build2/transcoding-apple-simd ${ARGS} &&\
+	@./build2/transcoding-cpu ${ARGS}&&\
+	@./build2/transcoding-gpu ${ARGS} &&\
+	@./build2/transcoding-xsimd ${ARGS} &&\
+	@./build2/transcoding-apple-simd ${ARGS}
 
--- openGLShading.cpp
+++ openGLShading.cpp
@@ -3,6 +3,59 @@
 #include <string>
 
 #include <simd/simd.h>
+#include <tbb/tbb.h>
+
+using namespace tbb;
+ 
+
+  constexpr std::size_t simd_size = 4;
+
+class InvertRendering {
+    const uint8_t * _texture1;
+    uint8_t * _output_buffer;
+public:
+    void operator()( const blocked_range<size_t>& r ) const {
+        const uint8_t * texture1 = _texture1;
+        uint8_t * output_buffer = _output_buffer;
+        for( size_t j=r.begin(); j!=r.end(); ++j )  {
+          auto i = j * simd_size;
+          auto rgb = simd_make_uint4(texture1[i], texture1[i + 1], texture1[i + 2], texture1[i + 3]);
+          auto res = 255 - rgb;
+          output_buffer[i] = res.x;
+          output_buffer[i + 1] = res.y;
+          output_buffer[i + 2] = res.z;
+          output_buffer[i + 3] = res.w;
+        }
+    }
+    InvertRendering(const uint8_t * texture1, uint8_t * output_buffer) :
+        _texture1(texture1), _output_buffer(output_buffer)
+    {}
+};
+
+class BlendRendering {
+    const uint8_t * _texture1;
+    const uint8_t * _texture2;
+    uint8_t * _output_buffer;
+public:
+    void operator()( const blocked_range<size_t>& r ) const {
+        const uint8_t * texture1 = _texture1;
+        const uint8_t * texture2 = _texture2;
+        uint8_t * output_buffer = _output_buffer;
+        for( size_t j=r.begin(); j!=r.end(); ++j )  {
+          auto i = j * simd_size;
+          auto rgb = simd_make_uint4(texture1[i], texture1[i + 1], texture1[i + 2], texture1[i + 3]);
+          auto rgb2 = simd_make_uint4(texture2[i], texture2[i + 1], texture2[i + 2], texture2[i + 3]);
+          auto res = (rgb2 + rgb) / 2;
+          output_buffer[i] = res.x;
+          output_buffer[i + 1] = res.y;
+          output_buffer[i + 2] = res.z;
+          output_buffer[i + 3] = res.w;
+        }
+    }
+    BlendRendering(const uint8_t * texture1, const uint8_t * texture2, uint8_t * output_buffer) :
+        _texture1(texture1), _texture2(texture2), _output_buffer(output_buffer)
+    {}
+};
 
 struct TextureInfo {
   const uint8_t *buffer;
@@ -47,32 +100,14 @@ void blendFrames(uint32_t texture1ID, uint32_t texture2ID) {
 
 void getCurrentResults(int width, int height, uint8_t *outputBuffer) {
   auto size = width * height * 3;
-  constexpr std::size_t simd_size = 4;
-  std::size_t vec_size = size - size % simd_size;
+  std::size_t vec_size = size / simd_size;
   auto texture1 = textures[0].buffer;
   auto texture2 = textures[1].buffer;
   if (next_invert) {
-    for(std::size_t i = 0; i < vec_size; i += simd_size)
-    {
-      auto rgb = simd_make_uint4(texture1[i], texture1[i + 1], texture1[i + 2], texture1[i + 3]);
-      auto res = 255 - rgb;
-      outputBuffer[i] = res.x;
-      outputBuffer[i + 1] = res.y;
-      outputBuffer[i + 2] = res.z;
-      outputBuffer[i + 3] = res.w;
-    }
+    parallel_for(blocked_range<size_t>(0, vec_size), InvertRendering(texture1, outputBuffer));
   }
   else {
-    for(std::size_t i = 0; i < vec_size; i += simd_size)
-    {
-      auto rgb = simd_make_uint4(texture1[i], texture1[i + 1], texture1[i + 2], texture1[i + 3]);
-      auto rgb2 = simd_make_uint4(texture2[i], texture2[i + 1], texture2[i + 2], texture2[i + 3]);
-      auto res = (rgb2 + rgb) / 2;
-      outputBuffer[i] = res.x;
-      outputBuffer[i + 1] = res.y;
-      outputBuffer[i + 2] = res.z;
-      outputBuffer[i + 3] = res.w;
-    }
+    parallel_for(blocked_range<size_t>(0, vec_size), BlendRendering( texture1,texture2, outputBuffer));
   }
 }
 
